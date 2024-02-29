e73e78021aae1d6df07080a2b7e366a98f8e81e4
JoniMonetti1 <jonathanmonetti6@gmail.com>
2024-02-28 21:30:35 -0300

    Se crean las clases necesarias en la logica, se crean las clases controladoras y se añade lombok como libreria para agilizar la creacion de getters, setters y constructores.

---
+++ src/main/java/org/example/logic/Duenio.java
@@ -0,0 +1,17 @@
+package org.example.logic;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Duenio {
+    private int idDuenio;
+    private String nombre;
+    private String celDuenio;
+}
---
+++ src/main/java/org/example/logic/Mascota.java
@@ -0,0 +1,23 @@
+package org.example.logic;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Mascota {
+    private int numCliente;
+    private String nombre;
+    private String raza;
+    private String color;
+    private String alergico;
+    private String atencionEspecial;
+    private String observaciones;
+    
+    private Duenio duenio;
+}
